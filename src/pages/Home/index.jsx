9dd4e912d451cc474946b2ebf3662ad1469d74a4
Evan Keith <evankeith@Evans-MacBook-Pro.local>
2022-11-09 09:23:56 -0800

    Add clear notification type form button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,36 +7,22 @@ import {
 } from 'react';
 import Notification from '../../components/Notification'
 import { NotificationsApi } from '@zeplin/sdk/dist/apis/notifications-api';
-const notificationTypes = [
-    "workspace.project", 
-    "workspace.styleguide",
-    "project.extension", 
-    "styleguide.extension",
-    "user.project_membership",
-    "user.styleguide_membership",
-    "project.component",
-    "styleguide.component",
-    "project.color",
-    "styleguide.color",
-    "project.screen.note",
-    "project.screen.note.comment", 
-    "project.screen",
-    "project.text_style",
-    "styleguide.text_style",
-    "project.spacing_token",
-    "styleguide.spacing_token",
-    "project.jira_attachment", 
-    "project.screen_section.jira_attachment",
-    "project.screen.jira_attachment", 
-    "project.screen.jira_integration",
-    "project.jira_integration",
-    "workspace.organization.member", 
-    "project.slack_integration", 
-    "styleguide.slack_integration", 
-    "project.member", 
-    "styleguide.member",
-    "project.flow_board"
-];
+const notificationTypes = {
+    'Project' : ["workspace.project"], 
+    'Styleguide': ["workspace.styleguide"], 
+    'Extensions': ["project.extension","styleguide.extension" ], 
+    'Membership': ["user.project_membership", "user.styleguide_membership"], 
+    'Components': ["project.component", "styleguide.component"],
+    'Colors': ["project.color", "styleguide.color"],
+    'Comments': ["project.screen.note", "project.screen.note.comment"],
+    'Screens': ["project.screen"],
+    'Text Styles': ["project.text_style", "styleguide.text_style"],
+    'Spacing Tokens': ["project.spacing_token", "styleguide.spacing_token"],
+    'Jira Integration': ["project.screen.jira_integration", "project.jira_integration"],
+    'Member': ["workspace.organization.member", "project.member", "styleguide.member"],
+    'Slack Integration': ["project.slack_integration", "styleguide.slack_integration"],
+    //'Flows': ["project.flow_board"]
+}
 
 
 
@@ -57,18 +43,25 @@ export default function Home() {
         })
     };
 
-    const changeType = (e) => {
+    const handleChangeType = (e) => {
+        e.preventDefault();
         const options = e.target.options;
         let value = [];
         for (let i = 0; i < options.length; i++) {
           if (options[i].selected) {
-            value.push(options[i].value);
+            value.push(notificationTypes[options[i].value]);
           }
         };
-        return setFilter(value);
+        return setFilter(value.flat());
     };
 
-    const filterUser = () => {
+    const handleClearSelected = (e) => {
+        e.preventDefault();
+        document.getElementById("typeForm").reset();
+        getAllNotifications();
+    }
+
+    const handleFilterUser = () => {
         return 
     }
     console.log(notifications);
@@ -78,22 +71,25 @@ export default function Home() {
         <div> 
             <h2>Home</h2>
             <div>
-                <form onChange={(e) => changeType(e)} >
+                <form id='typeForm' onChange={(e) => handleChangeType(e)}>
                     <label for="type">Choose a notification type:</label>
                     <select name="types" id="types" multiple size='10'>
-                        {notificationTypes.map((type, idx) => {
+                        {Object.keys(notificationTypes).map((type, idx) => {
                             return (<option value={type} key={idx}>{type}</option>)
                         })}
                     </select>
+                    <button onClick={(e) => handleClearSelected(e)}>Clear</button>
                 </form>
-                <form>
-                    <label for="user">Filter by user:</label>
-                    <select name="users" id="users" multiple size='5'>
-                        {notificationActors.map((type, idx) => {
-                            return (<option value={type} key={idx}>{type}</option>)
-                        })}
-                    </select>
-                </form>
+                {notificationActors.length  == 0 ? (<></>) : (
+                    <form>
+                        <label for="user">Filter by user:</label>
+                        <select name="users" id="users" multiple size='5'>
+                            {notificationActors.map((type, idx) => {
+                                return (<option value={type} key={idx}>{type}</option>)
+                            })}
+                        </select>
+                    </form>
+                )}
             </div>
             <div> 
                 <h3>Notifications</h3>
